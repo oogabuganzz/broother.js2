3847f0402f60faddd173dcd50b80c81b15f04201
oogabunganzz <nathan_zheng_zhien@xinminss.edu.sg>
2026-07-09 10:57:01 +0000

    Jul 9, 2026, 10:57 AM

--- L01_redCircle.js
+++ L01_redCircle.js
@@ -1,9 +1,12 @@
 function setup(){
-    createCanvas(600, 400);
-    background(220);
+    createCanvas(600, 600);
+    background('gray')
 }
-
 function draw(){
-    fill(255, 0, 0);
-    ellipse(300,200,200,200);
+    fill('green');
+    ellipse(300,100,200,200);
+    fill('yellow');
+    ellipse(300,300,200,200);
+    fill('red');
+    ellipse(300,500,200,200);
 }
--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -1,13 +1,30 @@
+let ball;
+
+
 function setup() {
   // Set up the canvas
-  new Canvas(800, 400);
+  new Canvas(400, 600);
   background(250); //background color
 
   // Basic shape testing
   // write your codes here
- 
-  // End Basic shape testing
-
+  noStroke();
+  ball = new Sprite();
+  ball.y = 50;
+  ball.x = 50;
+  ball.diameter = 50;
+  ball.color = "red";
+  ball.vel.y = 50;
+  ball.vel.x = 50;
+  // End Basic shape testing;
+  square = new Sprite(100,500,150,10);
+  square.color = "black";
+  yes = new Sprite(150,500,50,10);
+  yes2 = new Sprite(300,300,50,40);
+  square2 = new Sprite(150,200,30,30);
+  square3= new Sprite(120,270,20,70);
+  square4= new Sprite(220,370,70,70);
+  square5= new Sprite(10,270,10,400);
    // Create a bouncing ball sprite
    // write your codes here
 
@@ -15,4 +32,14 @@ function setup() {
 
 function draw() {
   // write your codes here
+  background(250);
+  textSize(16);
+  text("ball:"+round(ball.x)+","+round(ball.y),20,50);
+
+  if (ball.y>600 - ball.diameter/2|| ball.y < 0 + ball.diameter/2){
+    ball.vel.y = -1 * ball.vel.y;
+  }
+  if (ball.x>400 - ball.diameter/2|| ball.x < 0 + ball.diameter/2){
+    ball.vel.x = -1 * ball.vel.x;
+  }
 }
