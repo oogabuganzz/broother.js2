a4a1013939df522b97101f54464c8d25d8c2c1a1
oogabunganzz <nathan_zheng_zhien@xinminss.edu.sg>
2026-07-16 10:39:43 +0000

    Jul 16, 2026, 10:39 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -15,7 +15,7 @@ function setup() {
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
 
-  for(let i = 1; i <= 10; i += 2){
+  for(let i = 0; i <= 10; i += 2){
     text(i,100,100 + i*25)
   }
 
