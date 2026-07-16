eee8b88a3472f1bc7a170b37a6e1efa0a52e106f
oogabunganzz <nathan_zheng_zhien@xinminss.edu.sg>
2026-07-16 10:57:59 +0000

    Jul 16, 2026, 10:57 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -26,16 +26,16 @@ for(let i = 0; i <= 20; i += 2){
   if (Age > 0 && Age < 2){
     console.log("I am a infant")
   }
-  else if( Age < 4){
+  else if( Age <= 4){
     console.log("I am a toddler")
   }
-  else if( Age < 13){
+  else if( Age <= 13){
     console.log("I am a child")
   }
-  else if(Age < 18){
+  else if(Age <= 18){
     console.log("I am a teen")
   }
-  else if(Age <= 14){
+  else if(Age = 14){
     console.log("I am a adult")
   }
 
