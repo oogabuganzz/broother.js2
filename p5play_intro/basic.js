e64b67b7a7f9442b0211aa7bae3ff1dbda7ecebb
oogabunganzz <nathan_zheng_zhien@xinminss.edu.sg>
2026-07-16 10:14:00 +0000

    Jul 16, 2026, 10:14 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -10,7 +10,7 @@ function setup() {
   let areaTriangle = base*height*0.5;
   console.log("area of triangle:" + areaTriangle);
   textSize(16);
-  text()
+  text("")
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
 
