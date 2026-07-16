9633efef781ab8e3380ea8920fe7f2d126d568fe
oogabunganzz <nathan_zheng_zhien@xinminss.edu.sg>
2026-07-16 10:42:04 +0000

    Jul 16, 2026, 10:42 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -15,9 +15,7 @@ function setup() {
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
 
-  for(let i = 0; i <= 10; i += 2){
-    text(i,100,100 + i*25);
-  }
+
 
   // --- Exercise: Age category classification ---
   // write your codes here
