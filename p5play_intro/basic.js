13004e1bbe3b4268cfd09ba82e0d201b7c2900cb
oogabunganzz <nathan_zheng_zhien@xinminss.edu.sg>
2026-07-16 10:38:14 +0000

    Jul 16, 2026, 10:38 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -16,7 +16,7 @@ function setup() {
   // write your codes here
 
   for(let i = 1; i <= 10; i += 2){
-    text(i=+ i,100,100)
+    text(i)
   }
   // --- Exercise: Age category classification ---
   // write your codes here
