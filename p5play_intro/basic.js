505a25a3a38b04f9c11394df20385d95671dd663
oogabunganzz <nathan_zheng_zhien@xinminss.edu.sg>
2026-07-16 10:10:16 +0000

    Jul 16, 2026, 10:10 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -7,7 +7,7 @@ function setup() {
   // write your codes here
   let base = 100;
   let height =100;
-  let areaTriangle
+  let areaTriangle = base
 
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
